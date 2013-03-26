f01727e5749514cb8ac8d2fa8c2dff7a48e2877f
BeanHsiang <xiangbin731@qq.com>
2013-03-27 00:07:51 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ChromiumHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.DownLoadProgress = new System.Windows.Forms.ProgressBar();
+            this.Current = new System.Windows.Forms.Label();
+            this.Total = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DownLoadProgress
+            // 
+            this.DownLoadProgress.Location = new System.Drawing.Point(10, 10);
+            this.DownLoadProgress.Name = "DownLoadProgress";
+            this.DownLoadProgress.Size = new System.Drawing.Size(250, 23);
+            this.DownLoadProgress.TabIndex = 0;
+            // 
+            // Current
+            // 
+            this.Current.AutoSize = true;
+            this.Current.Location = new System.Drawing.Point(85, 36);
+            this.Current.Name = "Current";
+            this.Current.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Current.Size = new System.Drawing.Size(17, 12);
+            this.Current.TabIndex = 1;
+            this.Current.Text = "0M";
+            this.Current.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Total
+            // 
+            this.Total.AutoSize = true;
+            this.Total.Location = new System.Drawing.Point(217, 36);
+            this.Total.Name = "Total";
+            this.Total.Size = new System.Drawing.Size(17, 12);
+            this.Total.TabIndex = 2;
+            this.Total.Text = "0M";
+            this.Total.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Current：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Total：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 51);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Total);
+            this.Controls.Add(this.Current);
+            this.Controls.Add(this.DownLoadProgress);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Chromium Download";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar DownLoadProgress;
+        private System.Windows.Forms.Label Current;
+        private System.Windows.Forms.Label Total;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ChromiumHelper
+{
+    enum HttpMethod
+    {
+        Post,
+        Get
+    }
+
+    public partial class Form1 : Form
+    {
+        const string USER_AGENT = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1212.0 Safari/537.2";
+        const string ACCEPT = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+        const string ACCEPT_CHARSET = "GBK,utf-8;q=0.7,*;q=0.3";
+        const string ACCEPT_ENCODING = "gzip,deflate,sdch";
+        const string ACCEPT_LANGUAGE = "zh-CN,zh;q=0.8";
+        const string CONTENT_TYPE = "application/x-www-form-urlencoded";
+        const string CHROME_VERSION = "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Win/LAST_CHANGE";
+        const string CHROME_DIRECT = "http://commondatastorage.googleapis.com/chromium-browser-snapshots/index.html?path=Win/{0}/";
+        const string CHROME_DOWNLOAD = "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Win/{0}/chrome-win32.zip";
+        const string ZIP_FILE = "chrome-win32.zip";
+        long total = 0;//文件总长度
+        int taskCount = 3;//线程数
+        long globalCurrent = 0;//累积下载长度
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            //取得最新的版本号
+            HttpWebResponse res = Requst(CHROME_VERSION, string.Empty);
+            string version = string.Empty;
+            using (var sw = new StreamReader(res.GetResponseStream()))
+            {
+                version = sw.ReadToEnd();
+            }
+            res.Close();
+
+            //请求下载文件，初始化界面内容
+            res = Requst(string.Format(CHROME_DOWNLOAD, version), string.Empty);
+            total = res.ContentLength;
+            res.Close();
+
+            DownLoadProgress.Maximum = (int)total;
+            DownLoadProgress.Minimum = 0;
+
+            Total.Text = string.Format("{0}M", (total / 1024.00 / 1024.00).ToString("##0.00"));
+
+            IList<Range> list = InitRange((int)total, taskCount);
+            IList<Task> tasks = new List<Task>();
+
+            File.Delete(ZIP_FILE);
+
+            foreach (var item in list)
+            {
+                var task = new Task((o) =>
+               {
+                   var item2 = (Range)o;
+                   HttpWebResponse subRes = Requst(string.Format(CHROME_DOWNLOAD, version), string.Empty, item2.from, item2.to);
+                   using (var sw = subRes.GetResponseStream())
+                   {
+                       using (var fs = new FileStream(ZIP_FILE, FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite))
+                       {
+                           fs.Seek(item2.from, SeekOrigin.Begin);
+                           int length = 0;
+                           byte[] byt = new byte[1000];
+                           while ((length = sw.Read(byt, 0, byt.Length)) > 0)
+                           {
+                               fs.Write(byt, 0, length);
+                               Interlocked.Add(ref globalCurrent, length);
+                               //globalCurrent += length;
+                               Current.Text = string.Format("{0}M", (globalCurrent / 1024.00 / 1024.00).ToString("##0.00"));
+                               DownLoadProgress.Value = (int)globalCurrent;
+                           }
+                       }
+                   }
+                   subRes.Close();
+               }, item);
+                tasks.Add(task);
+                task.Start();
+            }
+
+            //另起一个任务，等待下载任务结束后弹出提示并主动关闭应用
+            var taskClose = new Task((o) =>
+            {
+                var t = (IList<Task>)o;
+                Task.WaitAll(t.ToArray());
+                if (globalCurrent == total)
+                {
+                    MessageBox.Show("Download finished !", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                this.Close();
+            }, tasks);
+            taskClose.Start();
+        }
+
+        //private void Update(object current)
+        //{
+        //    int c = (int)current;
+        //    Current.Text = ((int)current).ToString();
+        //    DownLoadProgress.Value = c;
+        //}
+
+        private IList<Range> InitRange(long total, int part)
+        {
+            IList<Range> list = new List<Range>();
+            long max = total;
+            while (max % part > 1000000) { max++; }
+            long division = max / part;
+            long last = -1;
+            for (int i = 0; i < part; i++)
+            {
+                Range range = new Range { from = last + 1, to = (i + 1) * division };
+                list.Add(range);
+                last = range.to;
+            }
+            Range r = list.Last<Range>();
+            r.to = total;
+            list.RemoveAt(list.Count - 1);
+            list.Add(r);
+            return list;
+        }
+
+        private HttpWebResponse Requst(string url, string param, long from, long to, HttpMethod method = HttpMethod.Get)
+        {
+            HttpWebRequest req = WebRequest.Create(url) as HttpWebRequest;
+            req.UserAgent = USER_AGENT;
+            req.Accept = ACCEPT;
+            req.ContentType = CONTENT_TYPE;
+            req.KeepAlive = true;
+            req.Method = method.ToString();
+            req.AllowAutoRedirect = true;
+            req.AddRange(from, to);
+            //req.Proxy = new WebProxy("127.0.0.1", 8888)
+            return req.GetResponse() as HttpWebResponse;
+        }
+
+        private HttpWebResponse Requst(string url, string param, HttpMethod method = HttpMethod.Get)
+        {
+            HttpWebRequest req = WebRequest.Create(url) as HttpWebRequest;
+            req.UserAgent = USER_AGENT;
+            req.Accept = ACCEPT;
+            req.ContentType = CONTENT_TYPE;
+            req.KeepAlive = true;
+            req.Method = method.ToString();
+            req.AllowAutoRedirect = true;
+            //req.Proxy = new WebProxy("127.0.0.1", 8888)
+            return req.GetResponse() as HttpWebResponse;
+        }
+
+    }
+
+    struct Range
+    {
+        public long from;
+        public long to;
+    }
+}
